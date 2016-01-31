54cf3c932aa2296a8b856cbad179c731dd419102
Rafael Costa <rafael.costa@wooga.net>
2016-01-31 11:20:13 +0100

    Creates the Animation Controller and config script

---
+++ Assets/TDG/Scripts/UI/CharacterAnimationController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class CharacterAnimationController : MonoBehaviour 
+{
+	public AnimationClip fallbackClip;
+
+	public List<TalkAnimConfig> talkConfig;
+	public List<ActionAnimConfig> actionConfig;
+	public List<EmotionAnimConfig> emotionConfig;
+}
+
+[System.Serializable]
+public class TalkAnimConfig
+{
+	public TalkCategory subcategory;
+	public AnimationClip clip;
+}
+
+[System.Serializable]
+public class ActionAnimConfig
+{
+	public ActionCategory subcategory;
+	public AnimationClip clip;
+}
+
+[System.Serializable]
+public class EmotionAnimConfig
+{
+	public EmotionCategory subcategory;
+	public AnimationClip clip;
+}
