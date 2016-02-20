594080f4d2bc6c71d2a9b7c6fd4e988c214b7b77
Valery Meleshkin <valery.meleshkin@gmail.com>
2016-02-20 17:27:04 +0100

    Mute button implementation based on the prefab by Holger

---
+++ Assets/TDG/Scripts/Util/MuteToggle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof (Toggle))]
+public class MuteToggle : MonoBehaviour {
+
+	void Start () {
+		var toggle = GetComponent<Toggle> ();
+		toggle.isOn = AudioController.Instance.isMuted;
+		toggle.onValueChanged.AddListener((bool muted) => AudioController.Instance.isMuted = muted);
+	}
+}
